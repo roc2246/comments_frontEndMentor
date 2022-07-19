12cf1483c1e58ea388a613d612a0effead8a3d01
Riley <riley.childs@yahoo.com>
2022-07-19 14:27:18 -0400

    Added alt attribute to images in .comment

--- js/script.js
+++ js/script.js
@@ -11,21 +11,21 @@ fetch(comments)
         "<div class='comment'>" +
           "<div class='comment__vote'>" +
             "<div>" +
-              "<img src='images/icon-plus.svg' alt='' class='upvote'>" +
+              "<img src='images/icon-plus.svg' alt='upvote score' class='upvote'>" +
             "</div>" +
             "<p class='rating'>" + data[x].score + "</p>" +
             "<div>" +
-              "<img src='images/icon-minus.svg' alt='' class='downvote'>" +
+              "<img src='images/icon-minus.svg' alt='downvote score' class='downvote'>" +
             "</div>" +
           "</div>" +
           "<div class='comment__top-row'>" +
-            "<img class='avatar' src='" + data[x].avatar_png + "'>" +
+            "<img class='avatar' alt='avatar' src='" + data[x].avatar_png + "'>" +
             "<span class='name'>" + data[x].username + "</span>" +
             "<span class='post-date'>" + data[x].createdAt + "</span>" +
           "</div>" +
           "<span class='reply-edit-delete'>" +
             "<span class='reply-edit-delete__reply'>" +
-            "<img src='images/icon-reply.svg' alt='' class='reply-edit-delete__reply--image'>Reply</span>" +
+            "<img src='images/icon-reply.svg' alt='reply icon' class='reply-edit-delete__reply--image'>Reply</span>" +
           "</span>" +
           "<div class='comment__text'>" +
             "<p>" + data[x].content + "</p>" +
