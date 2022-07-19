b56e1f6a15e87e910ddf1a3b7970ab3c2b02c18b
Riley <riley.childs@yahoo.com>
2022-07-19 14:25:17 -0400

    Formatted code for comments

--- js/script.js
+++ js/script.js
@@ -9,37 +9,27 @@ fetch(comments)
     for (let x in data) {
       commentBox.innerHTML +=
         "<div class='comment'>" +
-        "<div class='comment__vote'>" +
-        "<div>" +
-        "<img src='images/icon-plus.svg' alt='' class='upvote'>" +
-        "</div>" +
-        "<p class='rating'>" +
-        data[x].score +
-        "</p>" +
-        "<div>" +
-        "<img src='images/icon-minus.svg' alt='' class='downvote'>" +
-        "</div>" +
-        "</div>" +
-        "<div class='comment__top-row'>" +
-        "<img class='avatar' src='" +
-        data[x].avatar_png +
-        "'>" +
-        "<span class='name'>" +
-        data[x].username +
-        "</span>" +
-        "<span class='post-date'>" +
-        data[x].createdAt +
-        "</span>" +
-        "</div>" +
-        "<span class='reply-edit-delete'>" +
-        "<span class='reply-edit-delete__reply'>" +
-        "<img src='images/icon-reply.svg' alt='' class='reply-edit-delete__reply--image'>Reply</span>" +
-        "</span>" +
-        "<div class='comment__text'>" +
-        "<p>" +
-        data[x].content +
-        "</p>" +
-        "</div>" +
+          "<div class='comment__vote'>" +
+            "<div>" +
+              "<img src='images/icon-plus.svg' alt='' class='upvote'>" +
+            "</div>" +
+            "<p class='rating'>" + data[x].score + "</p>" +
+            "<div>" +
+              "<img src='images/icon-minus.svg' alt='' class='downvote'>" +
+            "</div>" +
+          "</div>" +
+          "<div class='comment__top-row'>" +
+            "<img class='avatar' src='" + data[x].avatar_png + "'>" +
+            "<span class='name'>" + data[x].username + "</span>" +
+            "<span class='post-date'>" + data[x].createdAt + "</span>" +
+          "</div>" +
+          "<span class='reply-edit-delete'>" +
+            "<span class='reply-edit-delete__reply'>" +
+            "<img src='images/icon-reply.svg' alt='' class='reply-edit-delete__reply--image'>Reply</span>" +
+          "</span>" +
+          "<div class='comment__text'>" +
+            "<p>" + data[x].content + "</p>" +
+          "</div>" +
         "</div>";
     }
   });
