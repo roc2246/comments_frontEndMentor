3ab9c322e9adb5ea35077378ac11bd0787745be3
Riley <riley.childs@yahoo.com>
2022-08-09 11:28:42 -0400

    Added fetch post to nmnew comment form

--- js/ajax.js
+++ js/ajax.js
@@ -1,12 +1,36 @@
 const newComment = () => {
-    const data= new FormData()
-  
-    const xhr = new XMLHttpRequest()
-    xhr.open("POST", "http://localhost:3000/add")
-    xhr.onload = ()=>{
-      console.log(this.response)
-    }
-    xhr.send(data)
-  
-    return false;
-  }
+  //     const xhr = new XMLHttpRequest()
+
+  //     xhr.open("POST", "http://localhost:3000/add")
+  // console.log(xhr.readyState, xhr.status)
+  //  xhr.onload = ()=>{
+  //         location.reload()
+  //     }
+  //     xhr.send()
+
+
+
+  return false;
+};
+//   https://www.youtube.com/watch?v=82hnvUYY6QA
+// https://www.tutsmake.com/node-js-get-and-send-data-from-ajax-request-example/#google_vignette
+const myForm = document.getElementById("new-comment")
+myForm.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+    const comment = document.getElementById("comment").value
+
+    fetch("http://localhost:3000/add", {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            comment_text: comment,
+        })
+    }).then((text) => {
+        console.log(text)
+    }).catch((error)=>{
+        console.log(error)
+    });
+
+
+})
