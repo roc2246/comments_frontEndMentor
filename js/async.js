a39fa5a4fad83a9dc090869a5651171f14eff5da
Riley <riley.childs@yahoo.com>
2022-08-09 11:31:45 -0400

    Cleaned up fetch code

--- js/async.js
+++ js/async.js
@@ -1,19 +1,4 @@
-const newComment = () => {
-  //     const xhr = new XMLHttpRequest()
 
-  //     xhr.open("POST", "http://localhost:3000/add")
-  // console.log(xhr.readyState, xhr.status)
-  //  xhr.onload = ()=>{
-  //         location.reload()
-  //     }
-  //     xhr.send()
-
-
-
-  return false;
-};
-//   https://www.youtube.com/watch?v=82hnvUYY6QA
-// https://www.tutsmake.com/node-js-get-and-send-data-from-ajax-request-example/#google_vignette
 const myForm = document.getElementById("new-comment")
 myForm.addEventListener('submit', (e)=> {
     e.preventDefault();
