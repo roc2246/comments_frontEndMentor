ccb28454cdc69f6fa601f7042bdb4d6e12a888b1
Riley <riley.childs@yahoo.com>
2022-08-27 16:03:10 -0400

    Heroku mysql test

--- js/login.js
+++ js/login.js
@@ -1,10 +1,26 @@
+const os = require('os')
+// console.log("______________________________")
+// console.log(os.hostname())
+
+// if (os.hostname() === "http://localhost:3000") {
+  // var host = "localhost";
+  // var user = "root";
+  // var password = "root";
+  // var database = "comments";
+// } else {
+  var host = "wh963069.ispot.cc";
+  var user = "childswe_commentsc";
+  var password = "qc4PsP5Y";
+  var database = "childswe_comments";
+// }
+
 // Create Connection
 const mysql = require("mysql");
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "comments",
-  });
-  
-  module.exports = db;
+  host: host,
+  user: user,
+  password: password,
+  database: database,
+});
+
+module.exports = db;
